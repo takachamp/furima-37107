a44562480102bc735cb8683086d0b526735dff65
takachamp <taka1991.mimi03.12251225@outlook.jp>
2021-11-05 19:21:39 +0900

    ユーザー新規登録の単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading, presence: true
-  validates :birthday, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-    validates :first_name
-    validates :last_name
+  with_options presence: true do
+    validates :nickname, :birthday, :password_confirmation
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'} 
+    validates :first_name_reading, :last_name_reading, format: {with:/\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters'}
   end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+ー\z/, message: 'is invalid. Input full-width katakana characters' } do
-    validates :first_name_reading
-    validates :last_name_reading
-  end
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
+  validates :password, :password_confirmation,
+  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'is invalid. Include both letters and numbers'}
 
   has_many :orders
   has_many :items
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    first_name { Gimei.first.kanji }
+    last_name { Gimei.last.kanji }
+    first_name_reading { Gimei.first.katakana }
+    last_name_reading { Gimei.last.katakana }
+    birthday {Faker::Date.birthday}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+  end
+end
