4e826cbc12957a9c0da415a7b9b16036a55453bf
takachamp <taka1991.mimi03.12251225@outlook.jp>
2021-11-08 11:35:01 +0900

    商品出品ページへのrootpathの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
+
   def index
+    @items = Item.all
+  end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
   end
 end
