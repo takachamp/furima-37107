8454abbfa1779156af4031ad075a1d6f631a2794
takachamp <taka1991.mimi03.12251225@outlook.jp>
2021-11-10 12:24:05 +0900

    商品削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -29,6 +29,14 @@ class ItemsController < ApplicationController
   def update
     if @item.update(item_params)
       redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to action: :index unless @item.user.id == current_user.id
     else
       render :edit
     end
