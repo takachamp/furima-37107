89bc316683ea08cb0e225e8f25f235f760f96d77
takachamp <taka1991.mimi03.12251225@outlook.jp>
2021-11-10 15:13:39 +0900

    herokuデプロイの際エラー原因の削除

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 2021_11_08_035203) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "item_info", null: false
